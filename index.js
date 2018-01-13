da30681eeb0b84798bc111a741c9ff3a6a67c5fe
Sharon Clapp <sbclapp@gmail.com>
2018-01-13 20:18:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ logWhisper("Hello")
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string){
     return `I can't hear you!`
-  }elseif (string.toUpperCase() === string){
+  }else if (string.toUpperCase() === string){
     return `YES INDEED!`
   }else{
     return `I love you, too.`
